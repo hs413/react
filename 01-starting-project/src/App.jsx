98960d47af26fe75dc0cf924d7da94e7978ed333
rtg <rtg2541@gmail.com>
2025-01-03 00:06:42 +0900

    starting project: 이벤트 독립 함수

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,8 +5,8 @@ import TabButton  from "./components/TabButton";
 
 
 function App() {
-  function handleSelect() {
-    console.log("hello select")
+  function handleSelect(selectedButton) {
+    console.log(selectedButton)
   }
 
   return (
@@ -24,10 +24,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
           </menu>
         </section>
       </div>
