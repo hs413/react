0f92094c946631377f111b71013aca61ebdaabad
rtg <rtg2541@gmail.com>
2025-01-04 00:02:15 +0900

    심화 프로젝트: 다차원 리스트 렌더링

---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
+
+export default function GameBoard() {
+  return (
+      <ol id="game-board">
+        {initialGameBoard.map((row, rowIdx) =>
+            <li key={rowIdx}>
+              <ol>
+                {row.map((playerSymbol, colIdx) =>
+                    <li key={colIdx}>
+                      <button>{playerSymbol}</button>
+                    </li>
+                )}
+              </ol>
+            </li>
+        )}
+      </ol>
+  )
+}
