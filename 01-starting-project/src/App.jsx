1af9b4be855e0a715c224fa1b813ccc3998717b4
rtg <rtg2541@gmail.com>
2025-01-03 21:08:46 +0900

    starting project: 조건부 렌더링

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -6,7 +6,7 @@ import {useState} from "react";
 import { EXAMPLES } from './data'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState('');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton)
@@ -32,15 +32,31 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {!selectedTopic ?
+              <p>Please select a topic</p>
+              :
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+          }
+          {!selectedTopic && <p>Please select a topic</p>}
+          {selectedTopic &&
+              <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>
+              </div>
+          }
         </section>
       </div>
   );
