494379d28429e8bad4095ff1065dfd546f96bfd0
rtg <rtg2541@gmail.com>
2025-01-03 23:54:29 +0900

    심화 프로젝트: 양방향 바인딩

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -8,16 +9,20 @@ export default function Player({ name, symbol }) {
     setIsEditing(editing => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
 
   if (isEditing) {
-    playerName = <input value={name} type="text" required />
+    editablePlayerName = <input value={playerName} type="text" required onChange={handleChange} />
   }
 
   return (
       <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
         <button onClick={handleEditClick}>{!isEditing ? 'Edit' : 'Save'}</button>
