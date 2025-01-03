a533654b1df6cdaf3fdc5a1641bd482f9e93db7a
rtg <rtg2541@gmail.com>
2025-01-04 00:09:39 +0900

    심화 프로젝트: 불변 객체로 상태 업데이트

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ]
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIdx, colIdx) {
+    setGameBoard((prevGameBoard) => {
+      // 잘못된 업데이트 방식
+      // prevGameBoard[rowIdx][colIdx] = 'X';
+      // return prevGameBoard
+
+      const updatedBoard = [...prevGameBoard.map(r => [...r])];;
+      updatedBoard[rowIdx][colIdx] = 'X';
+      return updatedBoard;
+    })
+  }
+
+
   return (
       <ol id="game-board">
-        {initialGameBoard.map((row, rowIdx) =>
+        {gameBoard.map((row, rowIdx) =>
             <li key={rowIdx}>
               <ol>
                 {row.map((playerSymbol, colIdx) =>
                     <li key={colIdx}>
-                      <button>{playerSymbol}</button>
+                      <button onClick={() => handleSelectSquare(rowIdx, colIdx)}>{playerSymbol}</button>
                     </li>
                 )}
               </ol>
