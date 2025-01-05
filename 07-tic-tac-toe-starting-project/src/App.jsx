a486a13257cba92676f80c9897e4b38d0d87eae1
rtg <rtg2541@gmail.com>
2025-01-05 14:25:36 +0900

    심화 프로젝트: 계산된 값 끌어올리기

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import {useState} from "react";
+import { WINNING_COMBINATIONS } from './winning-combinations.js'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
@@ -15,9 +22,16 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
 
   function handleSelectSquare(rowIdx, colIdx) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -40,7 +54,7 @@ function App() {
             <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
             <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <Log turns={gameTurns}/>
       </main>
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,46 +1,20 @@
 import {useState} from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
 
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  //
-  // function handleSelectSquare(rowIdx, colIdx) {
-  //   setGameBoard((prevGameBoard) => {
-  //     // 잘못된 업데이트 방식
-  //     // prevGameBoard[rowIdx][colIdx] = 'X';
-  //     // return prevGameBoard
-  //
-  //     const updatedBoard = [...prevGameBoard.map(r => [...r])];;
-  //     updatedBoard[rowIdx][colIdx] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   })
-  //
-  //   onSelectSquare();
-  // }
+export default function GameBoard({ onSelectSquare, board }) {
 
 
   return (
       <ol id="game-board">
-        {gameBoard.map((row, rowIdx) =>
+        {board.map((row, rowIdx) =>
             <li key={rowIdx}>
               <ol>
                 {row.map((playerSymbol, colIdx) =>
                     <li key={colIdx}>
-                      <button onClick={() => onSelectSquare(rowIdx, colIdx)}>{playerSymbol}</button>
+                      <button
+                          onClick={() => onSelectSquare(rowIdx, colIdx)}
+                          disabled={playerSymbol !== null}
+                      >{playerSymbol}</button>
                     </li>
                 )}
               </ol>
