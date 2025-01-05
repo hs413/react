0ef4bc3a0b681027c86b474d10201c6dd9da2803
rtg <rtg2541@gmail.com>
2025-01-05 17:28:03 +0900

    refs & portals: 참조 전달(forwardRef)

--- 07-refs-portals/src/components/TimerChallenge.jsx
+++ 07-refs-portals/src/components/TimerChallenge.jsx
@@ -3,13 +3,15 @@ import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
-
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -17,12 +19,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
   function handleStop() {
     clearTimeout(timer.current);
-    setTimerStarted(false);
   }
 
   return (
       <>
-        {timerExpired && <ResultModal targetTime={targetTime} result="lost"/>}
+        <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
         <section className="challenge">
           <h2>{title}</h2>
           <p className="challenge-time">
