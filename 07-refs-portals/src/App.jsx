0c319bc2e3f665c52cc656cd2ac980166edfbf85
rtg <rtg2541@gmail.com>
2025-01-05 17:11:44 +0900

    refs & portals: 타이머 참조

--- 07-refs-portals/src/App.jsx
+++ 07-refs-portals/src/App.jsx
@@ -1,5 +1,5 @@
 import Player from './components/Player.jsx';
-import {useState} from "react";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
 
@@ -7,7 +7,12 @@ function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
