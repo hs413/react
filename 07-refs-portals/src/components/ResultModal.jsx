5ce3071f16c7afd730ef1936781e4a1749039c4c
rtg <rtg2541@gmail.com>
2025-01-05 17:59:58 +0900

    refs & portals: 포탈 - HTML내에서 위치를 변경한다

--- 07-refs-portals/src/components/ResultModal.jsx
+++ 07-refs-portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef} from "react";
+import {createPortal} from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef();
@@ -15,20 +16,21 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     }
   })
 
-  return (
-      <dialog ref={dialog} className="result-modal">
+  return createPortal(<dialog ref={dialog} className="result-modal">
         {userLost && <h2>You lost </h2>}
         {!userLost && <h2>You Score: {score} </h2>}
         <p>
-        The target time was <strong>{targetTime}</strong>
+          The target time was <strong>{targetTime}</strong>
         </p>
         <p>
-          You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong>
+          You stopped the timer with <strong>{formattedRemainingTime} seconds
+          left</strong>
         </p>
         <form method="dialog" onSubmit={onReset}>
           <button>Close</button>
         </form>
-      </dialog>
+      </dialog>,
+      document.getElementById('modal')
   )
 })
 
