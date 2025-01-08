a676cb0e3bba76c1c45779d7e0df05472762fa87
rtg <rtg2541@gmail.com>
2025-01-08 21:41:21 +0900

    http 요청: fetch 데이터 변환 및 코드 분리

--- 15-http-request/src/components/AvailablePlaces.jsx
+++ 15-http-request/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 import Error from "./Error.jsx";
+import {sortPlacesByDistance} from "../loc.js";
+import {fetchAvailablePlaces} from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -11,18 +13,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch places')
-        }
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+              places,
+              position.coords.latitude,
+              position.coords.longitude
+          )
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        })
+
       } catch (e) {
         setError({ message: e.message || 'Could not fetch places'});
+        setIsFetching(false);
       }
 
-      setIsFetching(false);
     }
 
     fetchPlaces();
