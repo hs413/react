9d525420957dc5fe68d95172fc120b7e689cd4fd
rtg <rtg2541@gmail.com>
2025-01-09 21:33:09 +0900

    forms: ref 이용한 입력 관리

--- 17-forms/src/components/Login.jsx
+++ 17-forms/src/components/Login.jsx
@@ -1,55 +1,46 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: '',
-  })
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(enteredValues);
-    console.log('submmit')
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    console.log(enteredPassword, enteredEmail)
   }
 
-  function handleChange(identifier, value) {
-    setEnteredValues(prevValues => {
-      return {
-        ...prevValues,
-        [identifier]: value,
-      }
-    })
-  }
+
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-              id="email" type="email" name="email"
-              onChange={(event) => handleChange('email', event.target.value)}
-              value={enteredValues.email}
-          />
+        <div className="control-row">
+          <div className="control no-margin">
+            <label htmlFor="email">Email</label>
+            <input
+                id="email" type="email" name="email" ref={email}
+
+            />
+          </div>
+
+          <div className="control no-margin">
+            <label htmlFor="password">Password</label>
+            <input
+                id="password" type="password" name="password" ref={password}
+
+            />
+          </div>
         </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-              id="password" type="password" name="password"
-              onChange={(event) => handleChange('password', event.target.value)}
-              value={enteredValues.password}
-          />
-        </div>
-      </div>
-
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
+        <p className="form-actions">
+          <button className="button button-flat">Reset</button>
+          <button className="button">Login</button>
+        </p>
+      </form>
   );
 }
