958260700ea242716c5193dd592047063bfa6218
rtg <rtg2541@gmail.com>
2025-01-10 20:55:57 +0900

    redux: 여러 state

--- 19-redux/src/components/Counter.js
+++ 19-redux/src/components/Counter.js
@@ -4,6 +4,7 @@ import {useDispatch, useSelector} from "react-redux";
 const Counter = () => {
   const disPatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
     disPatch({ type: 'increment' })
@@ -17,12 +18,14 @@ const Counter = () => {
     disPatch({ type: 'decrement' })
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    disPatch({ type: 'toggle' })
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 10</button>
