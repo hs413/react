54807b6afb07b01f32bfe9dff89b75b61232eebb
rtg <rtg2541@gmail.com>
2025-01-11 15:04:51 +0900

    react router: 동적 라우팅

--- 21-router/src/App.js
+++ 21-router/src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage />},
       { path: '/products', element: <ProductsPage />},
+      { path: '/products/:productId', element: <ProductDetailPage />},
     ]
   }
 ])
--- 21-router/src/pages/Products.js
+++ 21-router/src/pages/Products.js
@@ -1,6 +1,25 @@
+import {Link} from "react-router-dom";
+
+const PRODUCTS = [
+  { id: 'p1', title: 'P 1'},
+  { id: 'p2', title: 'P 2'},
+  { id: 'p3', title: 'P 3'},
+]
+
 function ProductsPage() {
   return (
-      <h1>Products Page</h1>
+      <>
+        <h1>Products Page</h1>
+        <ul>
+          {PRODUCTS.map(prod => (
+              <li key={prod.id}>
+                <Link to={`/products/${prod.id}`}>
+                  {prod.title}
+                </Link>
+              </li>
+          ))}
+        </ul>
+      </>
   )
 }
 
