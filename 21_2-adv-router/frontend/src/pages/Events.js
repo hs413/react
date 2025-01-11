8ec404f6ef6321d2e8d4ca1bbb184b8e5f14499c
rtg <rtg2541@gmail.com>
2025-01-11 16:01:35 +0900

    avd react router: loader 함수 위치 조정

--- 21_2-adv-router/frontend/src/pages/Events.js
+++ 21_2-adv-router/frontend/src/pages/Events.js
@@ -6,38 +6,18 @@ import {useLoaderData} from "react-router-dom";
 function EventsPage() {
   const events = useLoaderData();
 
-  return (
-      <EventsList events={events} />
-  )
-  /*const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
+  return <EventsList events={events} />
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        setError('Fetching events failed.');
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
-  return (
-      <>
-        <div style={{ textAlign: 'center' }}>
-          {isLoading && <p>Loading...</p>}
-          {error && <p>{error}</p>}
-        </div>
-        {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-      </>
-  );*/
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+  if (!response.ok) {
+    // setError('Fetching events failed.');
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
