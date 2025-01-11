1848369a2c002ced49e16acfd82ad8d8022e7176
rtg <rtg2541@gmail.com>
2025-01-11 20:00:16 +0900

    avd react router: Form과 action 함수

--- 21_2-adv-router/frontend/src/App.js
+++ 21_2-adv-router/frontend/src/App.js
@@ -3,7 +3,7 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, { loader as eventDetailLoader } from "./pages/EventDetail";
 import EditEventPage from "./pages/EditEvent";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootPage from "./pages/Root";
 import EventsRoot from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
@@ -24,7 +24,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: 'new', element: <NewEventPage/> },
+          {
+            path: 'new',
+            element: <NewEventPage/>,
+            action: newEventAction,
+          },
           {
             path: ':eventId',
             id: 'event-detail',
