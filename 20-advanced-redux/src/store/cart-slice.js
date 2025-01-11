fdf323f295d3218023b771295c0c85f85bcb97e5
rtg <rtg2541@gmail.com>
2025-01-11 12:08:55 +0900

    advanced redux: thunk(액션 생성자) 활용

--- 20-advanced-redux/src/store/cart-slice.js
+++ 20-advanced-redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice } from '@reduxjs/toolkit'
+import {uiActions} from "./ui-slice";
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -39,6 +40,48 @@ const cartSlice = createSlice({
 
   }
 })
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending..',
+      message: 'Sending cart data',
+    }))
+
+    const sendRequest = async () => {
+      const response = await fetch(
+          '',
+          {
+            method: 'PUT',
+            body: JSON.stringify(cart),
+          }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed')
+      }
+    }
+
+    try {
+      const response = await sendRequest();
+
+      // const responseData = await response.json();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'sending cart data Success',
+      }))
+    } catch (e) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed',
+      }))
+    }
+  }
+}
 
 export const cartActions = cartSlice.actions;
 export default cartSlice
