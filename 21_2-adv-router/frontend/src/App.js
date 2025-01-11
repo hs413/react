f07000379f86da310f2098d216ddd76c4e16d478
rtg <rtg2541@gmail.com>
2025-01-11 15:55:52 +0900

    avd react router: loader를 이용하여 데이터 가져오기

--- 21_2-adv-router/frontend/src/App.js
+++ 21_2-adv-router/frontend/src/App.js
@@ -17,7 +17,19 @@ const router = createBrowserRouter([
         path: 'events',
         element: <EventsRoot />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async() => {
+              const response = await fetch('http://localhost:8080/events');
+              if (!response.ok) {
+                // setError('Fetching events failed.');
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            }
+          },
           { path: 'new', element: <NewEventPage/> },
           { path: ':id', element: <EventDetailPage /> },
           { path: ':id/edit', element: <EditEventPage /> },
