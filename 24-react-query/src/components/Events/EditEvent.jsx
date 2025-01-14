d4f86d925bee6eb91b5f968f7ac35cc663e60244
rtg <rtg2541@gmail.com>
2025-01-14 21:34:31 +0900

    react query: 쿼리 재사용 캐시

--- 24-react-query/src/components/Events/EditEvent.jsx
+++ 24-react-query/src/components/Events/EditEvent.jsx
@@ -1,10 +1,42 @@
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate, useParams} from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import {useQuery} from "@tanstack/react-query";
+import {fetchEvent} from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  })
+
+  let content
+
+  if (isPending) {
+    content = (
+        <div className="center" >
+          <LoadingIndicator />
+        </div>
+    )
+  }
+
+  if (data) {
+    content = (
+        <EventForm inputData={data} onSubmit={handleSubmit}>
+          <Link to=".." className="button-text">
+            Cancel
+          </Link>
+          <button type="submit" className="button">
+            Update
+          </button>
+        </EventForm>
+    )
+  }
 
   function handleSubmit(formData) {}
 
@@ -14,14 +46,7 @@ export default function EditEvent() {
 
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to=".." className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
