87c96df5b77cb122930f21a1fa24f37c470a7bed
rtg <rtg2541@gmail.com>
2025-01-14 23:06:35 +0900

    react query: 리액트 라우터 사용(선택사항)

--- 24-react-query/src/components/Events/EditEvent.jsx
+++ 24-react-query/src/components/Events/EditEvent.jsx
@@ -1,72 +1,71 @@
-import {Link, useNavigate, useParams} from 'react-router-dom';
+import {
+  Link,
+  redirect,
+  useNavigate, useNavigation,
+  useParams,
+  useSubmit
+} from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import {useMutation, useQuery} from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
 import {fetchEvent, queryClient, updateEvent} from "../../util/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { state } = useNavigation();
   const params = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data } = useQuery({
     queryKey: ['events', params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   })
 
   let content
 
-  if (isPending) {
-    content = (
-        <div className="center" >
-          <LoadingIndicator />
-        </div>
-    )
-  }
-
   if (data) {
     content = (
         <EventForm inputData={data} onSubmit={handleSubmit}>
-          <Link to=".." className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Update
-          </button>
+          {state === 'submitting' ? <p>Sending data...</p> : (
+              <>
+                <Link to=".." className="button-text">
+                  Cancel
+                </Link>
+                <button type="submit" className="button">
+                  Update
+                </button>
+              </>
+          )}
         </EventForm>
     )
   }
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      const newEvent = data.event;
-
-      await queryClient.cancelQueries({queryKey: ['events', params.id]});
-      const previousEvent = queryClient.getQueryData(['events', params.id]);
-
-      queryClient.setQueriesData(['events', params.id], newEvent);
-
-      return {
-        previousEvent
-      }
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(['events', params.id], context.previousEvent);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(['events', params.id]);
-    }
-
-  })
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     const newEvent = data.event;
+  //
+  //     await queryClient.cancelQueries({queryKey: ['events', params.id]});
+  //     const previousEvent = queryClient.getQueryData(['events', params.id]);
+  //
+  //     queryClient.setQueriesData(['events', params.id], newEvent);
+  //
+  //     return {
+  //       previousEvent
+  //     }
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(['events', params.id], context.previousEvent);
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(['events', params.id]);
+  //   }
+  //
+  // })
+  const submit = useSubmit();
 
   function handleSubmit(formData) {
-    mutate({
-      id: params.id,
-      event: formData,
-    })
-    navigate('../');
+    submit(formData, { method: 'PUT' })
   }
 
   function handleClose() {
@@ -78,4 +77,20 @@ export default function EditEvent() {
       {content}
     </Modal>
   );
+}
+
+export function loader({ params }) {
+  return queryClient.fetchQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  });
+}
+
+export async function action({request, params}) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+
+  await updateEvent({id: params.id, event: updatedEventData});
+  await queryClient.invalidateQueries(['events']);
+  return redirect('../');
 }
