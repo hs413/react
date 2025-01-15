00718bc4b576f8ee4f964fb63906565bfcf7d0fa
rtg <rtg2541@gmail.com>
2025-01-15 22:48:11 +0900

    연습프로젝트: 데이터 전송

--- 18-order-project/src/components/Checkout.jsx
+++ 18-order-project/src/components/Checkout.jsx
@@ -17,14 +17,35 @@ export default function Checkout() {
   function handleClose() {
     userProgressCtx.hideCheckout();
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        }
+      })
+    })
+  }
+
   return (
       <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2>Checkout</h2>
           <p>Total: {currencyFormatter.format(cartTotal)}</p>
 
-          <Input label="full name" type="text" id="full-name"/>
+          <Input label="full name" type="text" id="name"/>
           <Input label="Email" type="email" id="email"/>
           <Input label="Street" type="text" id="street"/>
           <div className="control-row">
